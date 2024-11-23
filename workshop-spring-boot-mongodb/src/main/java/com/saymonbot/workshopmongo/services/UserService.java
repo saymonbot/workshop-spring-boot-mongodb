7539ea601fd85bfd26f48004c456518ec3d91f26
unknown <saymon_botelho123@outlook.com>
2024-11-23 19:24:01 -0300

    User by id

--- workshop-spring-boot-mongodb/src/main/java/com/saymonbot/workshopmongo/services/UserService.java
+++ workshop-spring-boot-mongodb/src/main/java/com/saymonbot/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.saymonbot.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.saymonbot.workshopmongo.domain.User;
 import com.saymonbot.workshopmongo.repository.UserRepository;
+import com.saymonbot.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));
 	}
 }
