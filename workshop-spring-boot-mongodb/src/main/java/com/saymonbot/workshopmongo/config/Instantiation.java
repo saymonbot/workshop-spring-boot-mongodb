0ab5278eee24fe8b2588316f7027c01a1a0775a7
unknown <saymon_botelho123@outlook.com>
2024-11-30 12:06:22 -0300

    Comments in Posts

--- workshop-spring-boot-mongodb/src/main/java/com/saymonbot/workshopmongo/config/Instantiation.java
+++ workshop-spring-boot-mongodb/src/main/java/com/saymonbot/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.saymonbot.workshopmongo.domain.Post;
 import com.saymonbot.workshopmongo.domain.User;
 import com.saymonbot.workshopmongo.dto.AuthorDTO;
+import com.saymonbot.workshopmongo.dto.CommentDTO;
 import com.saymonbot.workshopmongo.repository.PostRepository;
 import com.saymonbot.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO com1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO com2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO com3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(com1, com2));
+		post2.getComments().addAll(Arrays.asList(com3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
