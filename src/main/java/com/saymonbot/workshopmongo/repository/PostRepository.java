1686dcff0f69f5906db5e76668f971aa8d1f90dd
saymonbot <saymon_botelho123@outlook.com>
2024-12-01 20:02:16 -0300

    Many Queries

--- src/main/java/com/saymonbot/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/saymonbot/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,9 @@
 package com.saymonbot.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -16,4 +16,7 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query(" { $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} }, { $or: [ { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
